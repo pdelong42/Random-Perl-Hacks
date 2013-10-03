0c21fc8a93522011c22ed16027c2833ff36e1571
Paul DeLong <pd72@nyu.edu>
2013-10-03 11:18:53 -0400

    variable name change; hepful commenting;

--- Idleness.pl
+++ Idleness.pl
@@ -1,20 +1,29 @@
 #!/usr/bin/perl
 
+=pod
+
+Just a quick-and-dirty script to print what fraction of the time the host has
+been idle, and how many days it's been up.  Not sure how useful this is past my
+one instance of needing it.  Since it relies on specifics of /proc, it only
+works on Linux.
+
+=cut
+
 use strict;
 use warnings;
 
-my $fh;
+my $hand;
 my $CPUs = 0;
 
-open( $fh, '/proc/uptime' )
+open( $hand, '/proc/uptime' )
    or die "unable to read /proc/uptime - aborting\n";
 
-my( $up, $idle ) = split ' ', readline( $fh );
+my( $up, $idle ) = split ' ', readline( $hand );
 
-open( $fh, '/proc/stat' )
+open( $hand, '/proc/stat' )
    or die "unable to read /proc/stat - aborting\n";
 
-foreach( readline $fh ) {
+foreach( readline $hand ) {
    ++$CPUs if m/^cpu\d+/;
 }
 
