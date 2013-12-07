7cf642205fe676454cb3dbaa91d4c979d9f69a2e
Paul DeLong <Paul.DeLong@gmail.com>
2013-12-06 23:15:10 -0500

    still in the non-destructive testing phase;

--- YouTubeNameFixer.pl
+++ YouTubeNameFixer.pl
@@ -7,11 +7,43 @@ use File::Find;
 use English qw( -no_match_vars );
 
 my $wanted = sub {
+
    return unless m{ ( .* ) \- ( .{11} ) \. ( webm | mp4 | flv ) }x;
-   print "$2 $3 $1\n";
+
+   my $ID = $2;
+   my @output = qx( ~/Stuff/bin/youtube-dl -s --get-filename http://www.youtube.com/watch?v=$ID 2> /dev/null );
+
+   if( $? < 0 ) {
+      print "ERROR: [$ID] failed to execute: $!\n";
+      return;
+   }
+
+   if( $? & 127 ) {
+      printf "ERROR: [$ID] child died with signal %d, %s coredump\n",
+           ( $? & 127 ),  ( $? & 128 ) ? 'with' : 'without';
+      return;
+   }
+
+   my $rc = $? >> 8;
+
+   unless( $rc == 0 ) {
+      printf "ERROR: [$ID] child exited with value $rc\n";
+      return;
+   }
+
+   my $new = shift @output;
+
+   chomp $new;
+
+   warn "WARNING: [$ID] expected only one line of output, but caught more"
+      if( scalar( @output ) > 0 );
+
+   #print "DEBUG: $ID [$3] $1\n";
+   #print "DEBUG: $ID $File::Find::fullname\n";
+   print "DEBUG: $ID $new\n";
 };
 
 push @ARGV, '.'
    unless @ARGV;
 
-find $wanted, @ARGV;
+find { wanted => $wanted, follow => 1 }, @ARGV;
