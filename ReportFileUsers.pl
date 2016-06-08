4d64bf80c50eccac2ca47a42f1dc7104434179cd
Paul DeLong <pdelong@amnh.org>
2016-06-08 13:49:33 -0400

    [feature/report_file_users] first stab at some real output;

--- ReportFileUsers.pl
+++ ReportFileUsers.pl
@@ -6,9 +6,10 @@ use warnings;
 use English '-no_match_vars';
 use Data::Dumper
 
-$Data::Dumper::Indent = 1;
+$Data::Dumper::Indent   = 1;
+$Data::Dumper::Sortkeys = 1;
 
-my( %NamesToPIDs, %PathsToPIDs );
+my( %PIDsToPaths, %PathsToPIDs, %PIDsToNames, %NamesToPIDs );
 
 foreach my $filename ( glob "/proc/[0123456789]*/maps" ) {
 
@@ -22,9 +23,13 @@ foreach my $filename ( glob "/proc/[0123456789]*/maps" ) {
    my( $PID ) = $filename =~ m{ ^ /proc/(\d+)/maps $ }x;
 
    foreach( readline $hand ) {
+
       my( $address, $perms, $offset, $dev, $inode, $pathname ) = split;
+
       next unless defined $pathname;
+
       ++$PathsToPIDs{ $pathname }{ $PID };
+      ++$PIDsToPaths{ $PID }{ $pathname };
    }
 }
 
@@ -48,12 +53,38 @@ foreach my $filename ( glob "/proc/[0123456789]*/stat" ) {
          unless "/proc/${PID}/stat" eq $filename;
    }
 
-   push @{ $NamesToPIDs{ $name } }, $PID;
+   ++$PIDsToNames{ $PID }{ $name };
+   ++$NamesToPIDs{ $name }{ $PID };
 }
 
-print Dumper \%PathsToPIDs;
+sub PrintJoinedHashes {
+
+   my( $hashref1, $hashref3 ) = @_;
+
+   foreach my $column1 ( sort keys %$hashref1 ) {
+
+      my $hashref2 = $hashref1->{ $column1 };
+
+      foreach my $column2 ( sort keys %$hashref2 ) {
+
+         my $hashref4 = $hashref3->{ $column2 };
+
+         foreach my $column3 ( sort keys %$hashref4 ) {
+
+            printf "$column1 $column2 $column3\n";
+         }
+      }
+   }
+}
+
+PrintJoinedHashes \%NamesToPIDs, \%PIDsToPaths;
+
+printf "\n";
+
+PrintJoinedHashes \%PathsToPIDs, \%PIDsToNames;
 
 =pod
+
 foreach( keys %NamesToPIDs ) {
    printf "%d x %s\n", scalar( @{ $NamesToPIDs{ $ARG } } ), $ARG;
 }
