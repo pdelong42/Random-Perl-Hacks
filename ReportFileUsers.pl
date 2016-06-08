0ba2bf490ae057de85df0d0383c81eff52f51250
Paul DeLong <pdelong@amnh.org>
2016-06-07 23:50:14 -0400

    [feature/report_file_users] tally-up the files in use

--- ReportFileUsers.pl
+++ ReportFileUsers.pl
@@ -4,8 +4,29 @@ use strict;
 use warnings;
 
 use English '-no_match_vars';
+use Data::Dumper
 
-my %NamesToPIDs;
+$Data::Dumper::Indent = 1;
+
+my( %NamesToPIDs, %PathsToPIDs );
+
+foreach my $filename ( glob "/proc/[0123456789]*/maps" ) {
+
+   my $hand;
+
+   unless( open $hand, $filename ) {
+      warn "unable to open $filename - skipping\n";
+      next;
+   }
+
+   my( $PID ) = $filename =~ m{ ^ /proc/(\d+)/maps $ }x;
+
+   foreach( readline $hand ) {
+      my( $address, $perms, $offset, $dev, $inode, $pathname ) = split;
+      next unless defined $pathname;
+      ++$PathsToPIDs{ $pathname }{ $PID };
+   }
+}
 
 foreach my $filename ( glob "/proc/[0123456789]*/stat" ) {
 
@@ -30,11 +51,13 @@ foreach my $filename ( glob "/proc/[0123456789]*/stat" ) {
    push @{ $NamesToPIDs{ $name } }, $PID;
 }
 
+print Dumper \%PathsToPIDs;
+
+=pod
 foreach( keys %NamesToPIDs ) {
    printf "%d x %s\n", scalar( @{ $NamesToPIDs{ $ARG } } ), $ARG;
 }
 
-=pod
 foreach( readline STDIN ) {
    
 }
