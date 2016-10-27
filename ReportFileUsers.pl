5ebab3964bee64054201e0805dd9cc87471f1563
Paul DeLong <Paul.DeLong@gmail.com>
2016-10-27 11:17:30 -0400

    updated/fixed the sample shell wrapper function;

--- ReportFileUsers.pl
+++ ReportFileUsers.pl
@@ -10,7 +10,7 @@ use Getopt::Long qw( :config no_ignore_case );
 
 an example of wrapping this in a Bourne Shell function:
 
-   function foo { rpm -q $* > /dev/null && rpm -ql $* | sudo ./ReportFileUsers.pl -s -p ; }
+   function foo { sudo sh -c "rpm -qa $* && rpm -qla $* | ./ReportFileUsers.pl -s -p && yum update $*" ; }
 
 Pass this the same args as you would to "sudo yum update ...".  Be
 sure to include all dependencies explicitly, or you won't get a
